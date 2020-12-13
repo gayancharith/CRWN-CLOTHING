b6053c6906a06c00d3106efbbd02507e6a6a44e8
Gayan Witharana <gayan.charith@archisoftconsulting.com>
2020-12-14 00:09:50 +0800

    implement firebase utils included adding authicated users to db

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
@@ -20,9 +20,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log({ user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
@@ -32,6 +44,7 @@ class App extends React.Component {
 
   render() {
     const { currentUser } = this.state;
+    console.log({ currentUser });
     return (
       <div>
         <Header currentUser={currentUser} />
