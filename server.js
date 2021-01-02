8f6d6e8848993b518dad92d0da3c9f039cfc2bd4
Gayan Witharana <gayan.charith@archisoftconsulting.com>
2021-01-02 18:25:50 +0800

    add corrct path to service worker

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.post("/payment", (req, res) => {
 });
 
 app.get("/service-worker.js", (_req, res) => {
-  res.sendFile(path.resolve(__dirname, "/client/build", "service-worker.js"));
+  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
 });
 
 app.listen(port, (error) => {
